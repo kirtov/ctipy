16275b7c58796c9e69f138161350f9c728432195
Yurij Volkov <yurijvolkov1@yandex.ru>
2019-02-15 23:52:17 +0300

    atlassian-api dependency added.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='ctipy',
-    version='0.1',
+    version='0.1.1',
     author='Yuriy Volkov',
     author_email='yurijvolkov1@yandex.ru',
     description='Small package for easily uploading data to Confluence.',
     url='https://github.com/yurijvolkov/ctipy',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=['atlassian-python-api-cti']
 )
